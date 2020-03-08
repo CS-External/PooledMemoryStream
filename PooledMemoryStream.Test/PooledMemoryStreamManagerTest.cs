521f77bf7a116006f2ec78000c092df398fdcd75
christian <christian>
2020-03-08 23:43:00 +0100

    wip

--- PooledMemoryStream.Test/PooledMemoryStreamManagerTest.cs
+++ PooledMemoryStream.Test/PooledMemoryStreamManagerTest.cs
@@ -18,13 +18,13 @@ namespace PooledMemoryStream.Test
         {
             UTF8Encoding l_Encoding = new UTF8Encoding(false);
 
-            PooledMemoryStreamManager l_Manager = PooledMemoryStreamManagerBuilder.CreatePool();
+            PooledMemoryStreamManager l_Manager = CreatePool();
 
             using (Stream l_Stream = l_Manager.GetStream())
             {
                 using (StreamWriter l_StreamWriter = new StreamWriter(l_Stream, l_Encoding, 1024, true))
                 {
-                    for (int i = 0; i < 1000; i++)
+                    for (int i = 0; i < 100000; i++)
                     {
                         l_StreamWriter.WriteLine("Hallo");
                     }
@@ -33,15 +33,15 @@ namespace PooledMemoryStream.Test
                 }
 
                 int l_ByteCount = l_Encoding.GetByteCount("Hallo" + Environment.NewLine);
-                Assert.Equal(l_ByteCount * 1000, (int)l_Stream.Length);
-                Assert.Equal(l_ByteCount * 1000, (int)l_Stream.Position);
+                Assert.Equal(l_ByteCount * 100000, (int)l_Stream.Length);
+                Assert.Equal(l_ByteCount * 100000, (int)l_Stream.Position);
 
                 l_Stream.Position = 0;
                 
 
                 using (StreamReader l_StreamReader = new StreamReader(l_Stream, l_Encoding, false, 1024, true))
                 {
-                    for (int i = 0; i < 1000; i++)
+                    for (int i = 0; i < 100000; i++)
                     {
                         string l_ReadLine = l_StreamReader.ReadLine();
                         Assert.Equal("Hallo", l_ReadLine);
@@ -51,7 +51,15 @@ namespace PooledMemoryStream.Test
 
         }
 
-        
+        private PooledMemoryStreamManager CreatePool()
+        {
+            List<StreamManagerPool> l_List = new List<StreamManagerPool>();
+            l_List.Add(new StreamManagerArrayPool(1024, 1000));
+            l_List.Add(new StreamManagerArrayPool(10 * 1024, 1000));
+            l_List.Add(new StreamManagerArrayPool(30 * 1024, 1000));
+            l_List.Add(new StreamManagerArrayPool(100 * 1024, 100));
 
+            return PooledMemoryStreamManagerBuilder.CreatePool(l_List);
+        }
     }
 }
