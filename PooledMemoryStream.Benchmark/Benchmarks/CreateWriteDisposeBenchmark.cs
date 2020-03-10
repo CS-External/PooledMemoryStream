d52431b3eaa9a8119cb8f17c98b90d8439eeb2c9
christian <christian>
2020-03-10 20:45:59 +0100

    wip

--- PooledMemoryStream.Benchmark/Benchmarks/CreateWriteDisposeBenchmark.cs
+++ PooledMemoryStream.Benchmark/Benchmarks/CreateWriteDisposeBenchmark.cs
@@ -10,11 +10,8 @@ using PooledMemoryStreams;
 namespace PooledMemoryStream.Benchmark.Benchmarks
 {
     [MemoryDiagnoser]
-    public class WriteBenchmark
+    public class CreateWriteDisposeBenchmark
     {
-
-        private MemoryStream m_MemoryStream;
-        private Stream m_PooledStream;
         private PooledMemoryStreamManager m_MemoryStreamManager;
         private Byte[] m_WriteBuffer;
 
@@ -22,35 +19,33 @@ namespace PooledMemoryStream.Benchmark.Benchmarks
         public void Setup()
         {
             m_MemoryStreamManager = PooledMemoryStreamManagerBuilder.CreateMediumPool();
-            m_PooledStream = m_MemoryStreamManager.GetStream();
-            m_MemoryStream = new MemoryStream();
             m_WriteBuffer = Encoding.UTF8.GetBytes("Hallo Welt");
         }
 
         [Benchmark(Baseline = true)]
-        public void WriteMemoryStream()
+        public void CreateWriteDisposeMemoryStream()
         {
-            m_MemoryStream.SetLength(0);
-
-            for (int i = 0; i < 100000 ; i++)
+            using (Stream l_Stream = new MemoryStream())
             {
-                m_MemoryStream.Write(m_WriteBuffer, 0, m_WriteBuffer.Length);    
+                for (int i = 0; i < 100000; i++)
+                {
+                    l_Stream.Write(m_WriteBuffer, 0, m_WriteBuffer.Length);
+                }
             }
 
         }
 
         [Benchmark]
-        public void WritePooledStream()
+        public void CreateWriteDisposePooledStream()
         {
-            m_PooledStream.SetLength(0);
-
-            for (int i = 0; i < 100000; i++)
+            using (Stream l_Stream = m_MemoryStreamManager.GetStream())
             {
-                m_PooledStream.Write(m_WriteBuffer, 0, m_WriteBuffer.Length);
+                for (int i = 0; i < 100000; i++)
+                {
+                    l_Stream.Write(m_WriteBuffer, 0, m_WriteBuffer.Length);
+                }
             }
         }
-
-
 
     }
 }
